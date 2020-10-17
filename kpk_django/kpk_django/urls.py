e28173b33ace9c98be162d15efd449cdf766284e
vldln-biven'мамы.net <strifee322@gmail.com>
2020-10-17 15:26:21 +0500

    index-catalog-basket

--- kpk_django/kpk_django/urls.py
+++ kpk_django/kpk_django/urls.py
@@ -1,6 +1,5 @@
 """kpk URL Configuration
-
-The urlpatterns list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
@@ -13,10 +12,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import  mainapp.views as mainapp
+import mainapp.views as mainapp
 from django.contrib import admin
 from django.urls import path
-
 
 urlpatterns = [
     path('', mainapp.index),
